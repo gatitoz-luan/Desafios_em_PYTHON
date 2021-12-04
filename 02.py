1d31875763eb946c984a2c7fd11e224c1f805b14
Luan Costa <luansilvacosta2010@hotmail.com>
2021-12-04 13:02:19 -0300

    Update 02.py

--- 02.py
+++ 02.py
@@ -3,26 +3,19 @@ while True:
     a,b,c= map(int,input().split())
     if a==b==c==0:
         break
-    else:
-        resposta=c1=comparador=0
-        for i in range(0,b):
-            v=input().split()
-            matriz.append(v)
-        for j in range(0,a):
-            for k in range(0,b):
-                if comparador==resposta:
-                    if matriz[k][j]=='1':
-                        comparador=resposta
-                        k1=k
-                        for k1 in range(k1,b):
-                            if matriz[k1][j]!='1':
-                                break
-                            c1+=1
-                            if c1==c:
-                                resposta+=1
-                else:
-                    k+=c1
-                    comparador=resposta
-                    c1=0    
-        print(resposta)
-        matriz.clear()
+    resposta=l=c1=0
+    for i in range(0,b):
+        v=input().split()
+        matriz.append(v)
+    for j in range(0,a):
+        for k in range(0,b):
+            if matriz[k][j]=='1':
+                for k in range(k,b):
+                    if matriz[k][j]!='1':
+                        break
+                    c1+=1
+                    if c1==c:
+                        resposta+=1
+                c1=0
+    print(resposta)
+    matriz.clear()
